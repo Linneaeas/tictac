4b330c5eed419ec3c0f4151f607b68db2db57dc6
Linnea <127832519+Linneaeas@users.noreply.github.com>
2023-10-15 13:36:45 +0200

    Winning Board Numbers & Switch player after each click

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -9,12 +9,23 @@ export class GameComponent implements OnInit{
 
   constructor(){};
   board!:Array<string>
-
+  player_turn="X";
+  winning_border=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [0,4,8],
+  ]
   ngOnInit(): void{
     this.board=new Array(9).fill("")
   };
 
   handlePlayer(index:number){
-    this.board[index]=index+"";
+    this.board[index]=this.player_turn
+    this.player_turn=(this.player_turn=="X")? "O":"X";
   }
 }

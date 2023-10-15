fe5b3a35a043c6eb29b9f5af8cc3b7a357ff7528
Linnea <127832519+Linneaeas@users.noreply.github.com>
2023-10-15 13:26:16 +0200

    Number becomes visible on Click in each Square

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css']
 })
-export class GameComponent {
+export class GameComponent implements OnInit{
 
+  constructor(){};
+  board!:Array<string>
+
+  ngOnInit(): void{
+    this.board=new Array(9).fill("")
+  };
+
+  handlePlayer(index:number){
+    this.board[index]=index+"";
+  }
 }
